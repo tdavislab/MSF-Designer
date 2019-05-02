f786a57ef74a1da8c5742f7496c8c97d509c3225
zhou325 <u1208920@utah.edu>
2019-05-02 16:20:01 -0600

    modify undo records

--- app/static/js/records.js
+++ app/static/js/records.js
@@ -10,9 +10,11 @@ class records{
     constructor(anim){
         this.anim = anim;
 
-        this.margin = {"top":20,"bottom":20,"left":10,"right":10};
+        this.margin = {"top":20,"bottom":20,"left":20,"right":20,"betweenstep":50};
         this.svgWidth = 1500;
-        this.svgHeight = 300;
+        this.svgHeight = 350;
+        this.frameWidth = 300;
+        this.frameHeight = 300;
         this.svg = d3.select("#undogroup").append("svg")
             .attr("id","undoSVG")
             .attr("width", this.svgWidth)
@@ -29,6 +31,45 @@ class records{
         this.addStep();
         console.log(this.stepRecorder)
 
+        this.xMap = d3.scaleLinear()
+            .domain([0, 1])
+            .range([0, this.frameWidth]);
+        this.yMap = d3.scaleLinear()
+            .domain([0, 1])
+            .range([0, this.frameHeight]);
+        this.curveMap = d3.line()
+            .x(d=>this.xMap(d.x))
+            .y(d=>this.yMap(d.y))
+            .curve(d3.curveCardinal.tension(0));
+
+        this.recordgroup1.append("rect")
+            .attr("x",this.margin.left)
+            .attr("y",this.margin.top)
+            .attr("width",this.frameWidth)
+            .attr("height",this.frameHeight)
+            .attr("stroke","rgb(44,123,246)")
+            .attr("fill", "none");
+        
+        this.recordgroup2.append("rect")
+            .attr("x",this.margin.left+this.frameWidth+this.margin.betweenstep)
+            .attr("y",this.margin.top)
+            .attr("width",this.frameWidth)
+            .attr("height",this.frameHeight)
+            .attr("stroke","rgb(44,123,246)")
+            .attr("fill", "none");
+        
+        this.recordgroup3.append("rect")
+            .attr("x",this.margin.left+2*this.frameWidth+2*this.margin.betweenstep)
+            .attr("y",this.margin.top)
+            .attr("width",this.frameWidth)
+            .attr("height",this.frameHeight)
+            .attr("stroke","rgb(44,123,246)")
+            .attr("fill", "none");
+        
+        this.drawStep(this.stepRecorder[0])
+            
+
+
     }
     
     addStep(){
@@ -38,8 +79,76 @@ class records{
     }
 
     drawStep(step){
+        let edgelist = d3.entries(step.edges);
         if(this.figureIdx===1){
             // select recordgroup1
+
+            
+            // drawing order matters
+            
+            let edges = this.recordgroup1.selectAll("path").data(edgelist);
+            edges.exit().remove();
+            let newedges = edges.enter().append("path");
+            edges = newedges.merge(edges);
+            edges
+                .attr("d",(d)=>{
+                    let d_new = d.value.slice(0,3);
+                    return this.curveMap(d_new);
+                })
+                .attr("class",(d)=>d.value[3]+"edge") // minedge/maxedge
+                .attr("transform","translate("+this.margin.left+","+this.margin.top+")")
+                // .attr("id",(d)=>d.key)
+                .style("fill", "none")
+                .style("stroke", "black")
+                .style("stroke-width",2)
+                .style("stroke-dasharray",(d)=>{
+                    if(d.value[3]==="max"){
+                        return "5,5";
+                    } else {return "";}
+                })
+
+            let circles = this.recordgroup1.selectAll("circle").data(step.cp);
+            circles.exit().remove();
+            let newcircles = circles.enter().append("circle");
+            circles = newcircles.merge(circles);
+            circles
+                .attr("cx",(d)=>this.xMap(d.x)+this.margin.left)
+                .attr("cy",(d)=>this.yMap(d.y)+this.margin.top)
+                .attr("r",15)
+                .attr("fill","white");
+            
+            let circletext = this.recordgroup1.selectAll("text").data(step.cp);
+            circletext.exit().remove();
+            let newcircletext = circletext.enter().append("text");
+            circletext = newcircletext.merge(circletext);
+            circletext
+                .attr('text-anchor', 'middle')
+                .attr('dominant-baseline', 'central')
+                .attr('font-size', '35px')
+                .attr("x",(d)=>this.xMap(d.x)+this.margin.left)
+                .attr("y",(d)=>this.yMap(d.y)+this.margin.top)
+                .attr("class",(d)=>{
+                    if(d.type==="max"){
+                        return "far max"
+                    } else if (d.type==="saddle"){
+                        return "far saddle"
+                    } else if (d.type==="min"){
+                        return "fas min"
+                    }
+                })
+                .text((d)=>{
+                        if(d.type==="max"){
+                            return "\uf192"
+                        } else if (d.type==="saddle"){
+                            return "\uf057"
+                        } else if (d.type==="min"){
+                            return "\uf140"
+                        }
+                })
+
+            
+            
+
             this.figureIdx = 2;
         } else if (this.figureIdx === 2){
             // select recordgroup2
@@ -51,6 +160,6 @@ class records{
     }
 
     undoStep(){
-        
+
     }
 }
