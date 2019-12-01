bed0d737459459dd601845a6962d56717b623f90
zhou325 <u1208920@utah.edu>
2019-11-30 19:42:55 -0700

    update run

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 from app import app
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080,debug=True)
+# if __name__ == '__main__':
+app.run(host='0.0.0.0',port=8080,debug=True)
